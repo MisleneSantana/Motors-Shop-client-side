06d9d8829cebc8f7ecf2d189ed2e26baa82a1123
Mislene Santana <contact.mislene@gmail.com>
2023-10-20 19:30:51 +0300

    add global style

---
+++ src/styles/theme.ts
@@ -0,0 +1,50 @@
+import { DefaultTheme } from 'styled-components';
+
+export const theme: DefaultTheme = {
+  colors: {
+    feedBack: {
+      feedbackAlert1: 'rgba(205, 43, 49, 1)',
+      feedbackAlert2: 'rgba(253, 216, 216, 1)',
+      feedbackAlert3: 'rgba(255, 229, 229, 1)',
+    },
+    success: {
+      feedbackSuccess1: 'rgba(24, 121, 78, 1)',
+      feedbackSuccess2: 'rgba(204, 235, 215, 1)',
+      feedbackSuccess3: 'rgba(221, 243, 228, 1)',
+    },
+    random: {
+      random1: ' rgba(227,77,140,1)',
+      random2: 'rgba(192,66,119,1)',
+      random3: ' rgba(125,42,77,1)',
+      random4: 'rgba(112,0,255,1)',
+      random5: 'rgba(98,0,227,1)',
+      random6: 'rgba(54,0,125,1)',
+      random7: 'rgba(52,153,116,1)',
+      random8: ' rgba(42,125,95,1)',
+      random9: 'rgba(21,61,46,1)',
+      random10: 'rgba(97,0,255,1)',
+      random11: 'rgba(87,0,227,1)',
+      random12: 'rgba(48,0,125,1)',
+    },
+    brand: {
+      brand1: 'rgba(69, 41, 230, 1)',
+      brand2: 'rgba(81, 38, 234, 1)',
+      brand3: 'rgba(176, 166, 240, 1)',
+      brand4: 'rgba(237, 234, 253, 1)',
+    },
+    greyScale: {
+      grey0: 'rgba(11,13,13,1)',
+      grey1: 'rgba(33,37,41,1)',
+      grey2: 'rgba(73,80,87,1)',
+      grey3: 'rgba(134,142,150,1)',
+      grey4: 'rgba(173,181,189,1)',
+      grey5: 'rgba(206,212,218,1)',
+      grey6: 'rgba(222,226,230,1)',
+      grey7: 'rgba(233,236,239,1)',
+      grey8: 'rgba(241,243,245,1)',
+      grey9: 'rgba(248,249,250,1)',
+      grey10: 'rgba(253,253,253,1)',
+      fixedWhiteFixed: 'rgba(255,255,255,1)',
+    },
+  },
+};
---
+++ src/styles/typography.ts
@@ -0,0 +1,86 @@
+import { DefaultTheme } from 'styled-components';
+
+export const typography: DefaultTheme = {
+  types: {
+    heading1_700: {
+      'font-size': '4.4rem', //44px
+      'font-weight': '700',
+      'line-height': '5.6rem', //56px
+    },
+    heading2_600: {
+      'font-size': '3.6rem', //36px
+      'font-weight': '600',
+      'line-height': '4.5rem', //45px
+    },
+    heading3_600: {
+      'font-size': '3.2rem', //32px
+      'font-weight': '600',
+      'line-height': '4rem', //40px
+    },
+    heading3_500: {
+      'font-size': '3.2rem', //32px
+      'font-weight': '500',
+      'line-height': '4rem', //40
+    },
+    heading4_600: {
+      'font-size': '2.8rem', //28px
+      'font-weight': '600',
+      'line-height': '3.5rem', //35px
+    },
+    heading4_500: {
+      'font-size': '2.8rem', //28px
+      'font-weight': '500',
+      'line-height': '3.5rem', //35px
+    },
+    heading5_600: {
+      'font-size': '2.4rem', //24px
+      'font-weight': '600',
+      'line-height': '3rem', //30px
+    },
+    heading5_500: {
+      'font-size': '2.4rem', //24px
+      'font-weight': '500',
+      'line-height': '3rem', //30px
+    },
+    heading6_600: {
+      'font-size': '2rem', //20px
+      'font-weight': '600',
+      'line-height': '2.5rem', //25px
+    },
+    heading6_500: {
+      'font-size': '2rem', //20px
+      'font-weight': '500',
+      'line-height': '2.5rem', //25px
+    },
+    heading7_600: {
+      'font-size': '1.6rem', //16px
+      'font-weight': '600',
+      'line-height': '2rem', //20px
+    },
+    heading7_500: {
+      'font-size': '1.6rem', //16px
+      'font-weight': '500',
+      'line-height': '2rem', //20px
+    },
+    body1_400: {
+      'font-size': '1.6rem', //16px
+      'font-weight': '400',
+      'line-height': '2.8rem', //28px
+    },
+    body1_600: {
+      'font-size': '1.6rem', //16px
+      'font-weight': '600',
+      'line-height': '2.8rem', //28px
+    },
+    body2_400: {
+      'font-size': '1.4rem', //14px
+      'font-weight': '400',
+      'line-height': '2.4rem', //24px
+    },
+    body2_500: {
+      'font-size': '1.4rem', //14px
+      'font-weight': '500',
+      'line-height': '2.4rem', //24px
+    },
+  },
+};
