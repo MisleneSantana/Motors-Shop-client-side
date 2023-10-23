ba77e9872d78920f8b271a85f6c09d49fcc40545
Mislene Santana <contact.mislene@gmail.com>
2023-10-23 10:42:25 +0300

    update style

--- src/styles/typography.ts
+++ src/styles/typography.ts
@@ -2,82 +2,82 @@ import { DefaultTheme } from 'styled-components';
 
 export const typography: DefaultTheme = {
   types: {
-    heading1_700: {
+    heading_700_44: {
       'font-size': '4.4rem', //44px
       'font-weight': '700',
       'line-height': '5.6rem', //56px
     },
-    heading2_600: {
+    heading_600_36: {
       'font-size': '3.6rem', //36px
       'font-weight': '600',
       'line-height': '4.5rem', //45px
     },
-    heading3_600: {
+    heading_600_32: {
       'font-size': '3.2rem', //32px
       'font-weight': '600',
       'line-height': '4rem', //40px
     },
-    heading3_500: {
+    heading_500_32: {
       'font-size': '3.2rem', //32px
       'font-weight': '500',
       'line-height': '4rem', //40
     },
-    heading4_600: {
+    heading_600_28: {
       'font-size': '2.8rem', //28px
       'font-weight': '600',
       'line-height': '3.5rem', //35px
     },
-    heading4_500: {
+    heading_500_28: {
       'font-size': '2.8rem', //28px
       'font-weight': '500',
       'line-height': '3.5rem', //35px
     },
-    heading5_600: {
+    heading_600_24: {
       'font-size': '2.4rem', //24px
       'font-weight': '600',
       'line-height': '3rem', //30px
     },
-    heading5_500: {
+    heading_500_24: {
       'font-size': '2.4rem', //24px
       'font-weight': '500',
       'line-height': '3rem', //30px
     },
-    heading6_600: {
+    heading_600_20: {
       'font-size': '2rem', //20px
       'font-weight': '600',
       'line-height': '2.5rem', //25px
     },
-    heading6_500: {
+    heading_500_20: {
       'font-size': '2rem', //20px
       'font-weight': '500',
       'line-height': '2.5rem', //25px
     },
-    heading7_600: {
+    heading_600_16: {
       'font-size': '1.6rem', //16px
       'font-weight': '600',
       'line-height': '2rem', //20px
     },
-    heading7_500: {
+    heading_500_16: {
       'font-size': '1.6rem', //16px
       'font-weight': '500',
       'line-height': '2rem', //20px
     },
-    body1_400: {
+    body_400_16: {
       'font-size': '1.6rem', //16px
       'font-weight': '400',
       'line-height': '2.8rem', //28px
     },
-    body1_600: {
+    body_600_16: {
       'font-size': '1.6rem', //16px
       'font-weight': '600',
       'line-height': '2.8rem', //28px
     },
-    body2_400: {
+    body_400_14: {
       'font-size': '1.4rem', //14px
       'font-weight': '400',
       'line-height': '2.4rem', //24px
     },
-    body2_500: {
+    body_500_14: {
       'font-size': '1.4rem', //14px
       'font-weight': '500',
       'line-height': '2.4rem', //24px
